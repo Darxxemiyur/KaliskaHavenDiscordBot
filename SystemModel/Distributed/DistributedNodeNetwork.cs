e9a453c18ae9c4b1e2d1df4d49af05114e2cf3a2
Darxxemiyur Bayfaderix <darxxie@bayfaderix.name>
2023-05-18 15:08:42 +0300

    Updated UI, laying framework to allow distributed running, creating tests.
    Signed-off-by: Darxxemiyur Bayfaderix <darxxie@bayfaderix.name>

---
+++ SystemModel/Distributed/DistributedNodeNetwork.cs
@@ -0,0 +1,55 @@
+﻿using Name.Bayfaderix.Darxxemiyur.Node.Network;
+
+using System.Runtime.Serialization;
+
+namespace KaliskaHaven.Distributed;
+
+/// <summary>
+/// Class of node result to either allow swapping of worker or keep it going on the same worker.
+/// </summary>
+public enum DNNNodeClass
+{
+	Free,
+	Keep
+}
+
+[Serializable]
+public abstract class DistributedNodeNetwork : IGeneralNodeNetwork, ISerializable
+{
+	protected DistributedNodeNetwork()
+	{
+	}
+
+	protected DistributedNodeNetwork(SerializationInfo info, StreamingContext context)
+	{
+	}
+
+	public abstract void GetObjectData(SerializationInfo info, StreamingContext context);
+
+	public abstract Task<IGeneralStepInfo?> GetStartStepInfo(IGeneralStepInfo? payload);
+
+	public abstract Task<IGeneralStepInfo?> GetStartStepInfo(object? payload = null);
+}
+
+[Serializable]
+public abstract class DistributedNodeNetworkNode : IGeneralStepInfo, ISerializable
+{
+	public abstract DNNNodeClass Class {
+		get;
+	}
+	protected DistributedNodeNetworkNode()
+	{
+	}
+
+	protected DistributedNodeNetworkNode(SerializationInfo info, StreamingContext context)
+	{
+	}
+
+	public abstract IGeneralNodeNetwork? ParentNetwork {
+		get;
+	}
+
+	public abstract void GetObjectData(SerializationInfo info, StreamingContext context);
+
+	public abstract Task<IGeneralStepInfo?> Run(IGeneralStepInfo? previous = null, CancellationToken token = default);
+}
